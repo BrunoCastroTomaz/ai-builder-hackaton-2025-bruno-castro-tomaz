8eee49e13d350fae0b2b65e999c91f94fd0cf83b
BrunoCastroTomaz <brunoworkjobs@gmail.com>
2025-10-25 12:46:58 -0300

    feat: add chat's interface and first business logics ; reaproveitando estrutura de código utilizada em projeto antigo

--- chatSAC/app.py
+++ chatSAC/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+cardapio = [
+    'portuguesa',
+    'calabresa',
+    'quatro queijos',
+    'atum com mussarela',
+    'mussarela',
+    'alho'
+]
+
+#app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+
+@app.route('/chatSAC', methods['POST'])
+def chatSAC():
+    #lógica de resposta verificando se há cardápio no sistema
+    data = request.get_json()
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home Page', message='Welcome to my basic Flask application!')
+    return render_template('index.html', title='ChatSAC')
 
 if __name__ == '__main__':    
     app.run(debug=True)
+
